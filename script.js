afd51db177fb21493d7172b49d451fb0a01371f3
Jörn Zaefferer <joern.zaefferer@gmail.com>
2020-10-02 20:30:39 +0200

    slightly friendlier feedback

--- script.js
+++ script.js
@@ -74,51 +74,54 @@ video.addEventListener("playing", async () => {
     const drawOptions = {
       anchorPosition: "TOP_LEFT",
       backgroundColor: "rgba(0, 0, 0, 0.5)",
+      fontSize: 20,
+      padding: 8,
     };
     if (resizedDetections.length === 0) {
-      text.push("Can't find any faces - are you there?");
-      text.push("Try moving your camera so that your face is centered");
+      text.push("Can't find any faces; Try moving your");
+      text.push("camera so that your face is centered");
       drawOptions.fontColor = "red";
     } else {
       const box = resizedDetections[0]._box;
       const positionLimit = 0.1;
       const distanceLimitClose = 0.45;
-      const distanceLimitFar = 0.25;
+      const distanceLimitFar = 0.21;
       // hiding at the bottom?
       if (box._y + box._height >= displaySize.height * (1 - positionLimit)) {
-        text.push("Found your face, but it's too close to the bottom edge");
-        text.push("Try moving your camera so that your face is centered");
+        text.push("Too close to the bottom edge");
+        // text.push("Try moving your camera so that your face is centered");
         drawOptions.fontColor = "yellow";
         // or the top?
       } else if (box._y <= displaySize.height * positionLimit) {
-        text.push("Found your face, but it's too close to the top edge");
-        text.push("Try moving your camera so that your face is centered");
+        text.push("Too close to the top edge");
+        // text.push("Try moving your camera so that your face is centered");
         drawOptions.fontColor = "yellow";
         // or the right?
       } else if (
         box._x + box._width >=
         displaySize.width * (1 - positionLimit)
       ) {
-        text.push("Found your face, but it's too close to the right edge");
-        text.push("Try moving your camera so that your face is centered");
+        text.push("Too close to the right edge");
+        // text.push("Try moving your camera so that your face is centered");
         drawOptions.fontColor = "yellow";
         // or the left?
       } else if (box._x <= displaySize.width * positionLimit) {
-        text.push("Found your face, but it's too close to the left edge");
-        text.push("Try moving your camera so that your face is centered");
+        text.push("Too close to the left edge");
+        // text.push("Try moving your camera so that your face is centered");
         drawOptions.fontColor = "yellow";
         // too close?
       } else if (box._width >= displaySize.width * distanceLimitClose) {
-        text.push("Found your face, but it's too close");
+        text.push("Too close");
         text.push("Try moving back a little");
         drawOptions.fontColor = "yellow";
         // too far away?
       } else if (box._width <= displaySize.width * distanceLimitFar) {
-        text.push("Found your face, but it's too far away");
+        text.push("Too far away");
         text.push("Try moving closer a little");
         drawOptions.fontColor = "yellow";
       } else {
         text.push("Looking good!");
+        drawOptions.fontSize = 30;
         drawOptions.fontColor = "#55ff07";
       }
       // console.log({
